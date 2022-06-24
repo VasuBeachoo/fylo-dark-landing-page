10b8c70580a6447b298e3fb3800ab7d6c8210c64
Vasu Beachoo <vasubeachoo@gmail.com>
2022-06-23 23:04:35 -0400

    Style cta section

--- src/components/CtaBlock.jsx
+++ src/components/CtaBlock.jsx
@@ -9,13 +9,23 @@ const CtaBlock = ({
     <div className={`${block}__container`}>
       <h3 className={`${block}__heading`}>{headingText}</h3>
       <p className={`${block}__paragraph`}>{paragraphText}</p>
-      <form className={`${block}__form`} novalidate>
+      <form className={`${block}__form`} noValidate>
         <div className={`${block}__form-container`}>
           <div className={`${block}__email-input-container`}>
-            <input type="email" className={`${block}__email-input`} />
-            <p className={`${block}__error-msg`}>{errorText}</p>
+            <input
+              type="email"
+              className={`${block}__email-input`}
+              placeholder="email@example.com"
+            />
+            <p className={`${block}__error-msg ${block}__error-msg--hidden`}>
+              {errorText}
+            </p>
           </div>
-          <input type="submit" value={btnText} className={`${block}__btn`} />
+          <input
+            type="submit"
+            value={btnText}
+            className={`${block}__submit-input`}
+          />
         </div>
       </form>
     </div>
