80ab0b75d96cd7dd8bcdac6d6cf75785f9de65a7
Vasu Beachoo <vasubeachoo@gmail.com>
2022-06-24 19:51:31 -0400

    Add validation for email input

--- src/components/CtaBlock.jsx
+++ src/components/CtaBlock.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const CtaBlock = ({
   block,
   headingText,
@@ -5,19 +7,41 @@ const CtaBlock = ({
   errorText,
   btnText,
 }) => {
+  const [validity, setValidity] = useState(true);
+
+  function handleSubmit(e) {
+    const input = document.querySelector(`.${block}__email-input`);
+    if (input.value === "" || !input.validity.valid) {
+      e.preventDefault();
+      setValidity(false);
+    } else {
+      setValidity(true);
+    }
+  }
+
   return (
     <div className={`${block}__container`}>
       <h3 className={`${block}__heading`}>{headingText}</h3>
       <p className={`${block}__paragraph`}>{paragraphText}</p>
-      <form className={`${block}__form`} noValidate>
+      <form
+        className={`${block}__form`}
+        onSubmit={(e) => handleSubmit(e)}
+        noValidate
+      >
         <div className={`${block}__form-container`}>
           <div className={`${block}__email-input-container`}>
             <input
               type="email"
-              className={`${block}__email-input`}
+              className={`${block}__email-input ${block}__email-input--${
+                validity ? "valid" : "invalid"
+              }`}
               placeholder="email@example.com"
             />
-            <p className={`${block}__error-msg ${block}__error-msg--hidden`}>
+            <p
+              className={`${block}__error-msg ${block}__error-msg--${
+                validity ? "hidden" : "visible"
+              }`}
+            >
               {errorText}
             </p>
           </div>
